48c8cf18467f0cbdad58e929e953979cdeb17c09
blackj20 <jswebolo3@gmail.com>
2026-07-28 20:55:28 +0100

    Expose les composants Skill Factory V0.5

--- kairos/skills/__init__.py
+++ kairos/skills/__init__.py
@@ -1,15 +1,30 @@
 """Construction, contrôle et activation de skills versionnées."""
 
 from .builder import SkillBuilder
+from .factory import SkillFactory
+from .integrity import ErreurIntegrite, artifact_digest
 from .manifest import SkillManifest
+from .models import ActivationResult, CandidateRecord, ValidationReport
+from .policy import PermissionPolicy, PermissionReport
 from .registry import SkillRegistry
 from .sandbox import SandboxRunner
-from .scanner import SkillScanner
+from .scanner import ScanReport, SkillScanner
+from .store import SkillFactoryStore
 
 __all__ = [
+    "ActivationResult",
+    "CandidateRecord",
+    "ErreurIntegrite",
+    "PermissionPolicy",
+    "PermissionReport",
     "SandboxRunner",
+    "ScanReport",
     "SkillBuilder",
+    "SkillFactory",
+    "SkillFactoryStore",
     "SkillManifest",
     "SkillRegistry",
     "SkillScanner",
+    "ValidationReport",
+    "artifact_digest",
 ]
