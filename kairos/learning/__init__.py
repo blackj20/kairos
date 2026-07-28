be718aacb3242716e39e1683b655612c4da04728
blackj20 <jswebolo3@gmail.com>
2026-07-28 19:28:57 +0100

    Expose la consolidation supervisée du créateur

--- kairos/learning/__init__.py
+++ kairos/learning/__init__.py
@@ -1,12 +1,15 @@
-"""Acquisition et extraction de sources structurées."""
+"""Acquisition, extraction et consolidation contrôlée des connaissances."""
 
 from .acquire import Acquire
+from .creator import CreatorLearningPipeline, CreatorLearningResult
 from .extract import Extractor
 from .internet import InternetAcquire, InternetDocument, UrlFetcher
 from .pipeline import InternetLearningPipeline, LearningResult
 
 __all__ = [
     "Acquire",
+    "CreatorLearningPipeline",
+    "CreatorLearningResult",
     "Extractor",
     "InternetAcquire",
     "InternetDocument",
